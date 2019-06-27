6d35cc51d804f5870d60a85d10643e09845302bb
Márcio França Lima <marciofrancalima@hotmail.com>
2019-06-27 12:28:46 -0300

    UserController added and first route to register user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    // Verifica se o usuário já existe
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existe' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
