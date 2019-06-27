f3ad0eaeeb519e77803fac5957ec4c9007919560
Márcio França Lima <marciofrancalima@hotmail.com>
2019-06-27 11:56:32 -0300

    Created loader of models and first user insert

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Bem vindo' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Márcio França Lima',
+    email: 'marciofrancalima@hotmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
