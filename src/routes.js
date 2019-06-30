a7fe321a48c88124d6d25496facf91064de990f9
Márcio França Lima <marciofrancalima@hotmail.com>
2019-06-30 19:47:41 -0300

    Added feature for storing files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,6 +18,10 @@ routes.post('/sessions', SessionController.store);
 // As rotas abaixo precisam de autenticação do usuário
 routes.use(authMiddleware);
 
+// Users
 routes.put('/users', UserController.update);
+
+// Files
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
