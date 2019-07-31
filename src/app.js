cf9be1366d56f3eb0b372437431ca373f2aaf062
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-31 18:27:35 -0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 import { resolve } from 'path';
 import routes from './routes';
 
@@ -14,6 +15,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
